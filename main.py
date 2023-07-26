113be1111bb8ee33df0367e8f4413fb298e399ac
Antonio Anon Brasolin <antonio.brasolin.tpv@one-line.com>
2023-07-26 07:47:40 +0000

    Add conflicting things

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title("My test app")
 
-DATE_COLUMN = "date/time"
+DATE_COLUMN = "date/time"  # Commit1
 DATA_URL = (
     "https://s3-us-west-2.amazonaws.com/"
     "streamlit-demo-data/uber-raw-data-sep14.csv.gz"
